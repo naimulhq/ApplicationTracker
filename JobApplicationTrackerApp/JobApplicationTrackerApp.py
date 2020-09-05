0254793c48c2497638deb4ab9fcd17d52a92755a
Naimul Hoque <naimul@ucsb.edu>
2020-09-05 13:14:40 -0700

    Changed Background color and created title page.

--- JobApplicationTrackerApp/JobApplicationTrackerApp.py
+++ JobApplicationTrackerApp/JobApplicationTrackerApp.py
@@ -1,20 +1,31 @@
 import kivy
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.lang import Builder
+from kivy.uix.relativelayout import RelativeLayout
 
 
-class jobTrackerTable(BoxLayout):
-    def __init__(self, **kwargs):
-        super(jobTrackerTable,self).__init__(**kwargs)
-        self.orientation = "vertical"
+class HomePage(RelativeLayout):
+    def __init__(self,**kwargs):
+        super(RelativeLayout,self).__init__(**kwargs)
+        # Add title for home screen
+        title = Label(text="Job Application Tracker", font_size='50sp')
+        title.color = [0,0,0,1]
+        # Add title widget to RelativeLayout
+        self.add_widget(title)
         
-       
 
 class JobTrackerApp(App):
     def build(self):
-        return jobTrackerTable()
+        # Set up the background color for the window
+        r,g,b = 255,245,245
+        Window.clearcolor = [float(r/255),float(g/255),float(b/255),1]
+        # Set up home screen for app
+        homeScreen = HomePage()
+        return homeScreen
+        
+        
+        
 
 if __name__ == "__main__":
     JobTrackerApp().run()
