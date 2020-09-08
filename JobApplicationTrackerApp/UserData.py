6fcd30370e525f5cf0ac83bbd93311ca3afb5cf3
Naimul Hoque <naimul@ucsb.edu>
2020-09-08 03:51:37 -0700

    Created a User Data class which represents the data acquired from app.

---
+++ JobApplicationTrackerApp/UserData.py
@@ -0,0 +1,6 @@
+class UserData:
+    def __init__(self, jobPosition, company, submittedData):
+        self.jobPosition = jobPosition
+        self.company = company
+        self.submittedData = submittedData
+    
