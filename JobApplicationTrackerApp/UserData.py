cff229cb9d7b2c47a4ef1a90825d333786fb5e31
Naimul Hoque <naimul@ucsb.edu>
2020-09-09 03:26:30 -0700

    Added a new class that will hold specific values for potential application.

--- JobApplicationTrackerApp/UserData.py
+++ JobApplicationTrackerApp/UserData.py
@@ -4,3 +4,9 @@ class UserData:
         self.company = company
         self.submittedData = submittedData
     
+class PotentialApps:
+    def __init__(self,jobPosition, company, location, submission):
+        self.jobPosition = jobPosition
+        self.company = company
+        self.location = location
+        self.submission = submission
